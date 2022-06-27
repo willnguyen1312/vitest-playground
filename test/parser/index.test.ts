687e3549f662ab136ec4eb02ab64d0e2e4b95b93
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-06-27 22:52:33 +0700

    🤞

--- test/parser/index.test.ts
+++ test/parser/index.test.ts
@@ -1,5 +1,5 @@
-// import { parse } from "acorn";
-import { parse } from "@babel/parser";
+import { parse } from "acorn";
+// import { parse } from "@babel/parser";
 import { readFileSync, writeFileSync } from "fs";
 import { resolve } from "path";
 
@@ -9,7 +9,7 @@ describe("Acorn parser", () => {
       encoding: "utf-8",
     });
 
-    const output = parse(source);
+    const output = parse(source, { ecmaVersion: 2022 });
     writeFileSync(
       resolve(__dirname, "out.json"),
       JSON.stringify(output, null, 2)
