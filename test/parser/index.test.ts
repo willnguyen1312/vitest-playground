db7ea0fac3d40b474b1742e45b099499f99a18cb
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-06-27 22:42:13 +0700

    ❤️‍🔥

---
+++ test/parser/index.test.ts
@@ -0,0 +1,18 @@
+// import { parse } from "acorn";
+import { parse } from "@babel/parser";
+import { readFileSync, writeFileSync } from "fs";
+import { resolve } from "path";
+
+describe("Acorn parser", () => {
+  it("should work", async () => {
+    const source = readFileSync(resolve(__dirname, "file.ts"), {
+      encoding: "utf-8",
+    });
+
+    const output = parse(source);
+    writeFileSync(
+      resolve(__dirname, "out.json"),
+      JSON.stringify(output, null, 2)
+    );
+  });
+});
